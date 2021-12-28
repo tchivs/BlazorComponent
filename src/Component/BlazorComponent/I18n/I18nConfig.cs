5c8c9a88ad71419073e7f7308507418261e60793
wuweilai <wuweilai@masastack.com>
2021-12-28 13:37:17 +0800

    i18n 重构

---
+++ src/Component/BlazorComponent/I18n/I18nConfig.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BlazorComponent.Components
+{
+    public class I18nConfig
+    {
+        CookieStorage? _cookieStorage;
+
+        public I18nConfig() { }
+
+        public I18nConfig(CookieStorage cookieStorage)
+        {
+            _cookieStorage = cookieStorage;
+        }
+
+        public static string LanguageCookieKey { get; set; } = "Masa_I18nConfig_Language";
+
+        private string? _language;
+
+        public string? Language
+        {
+            get => _language;
+            set
+            {
+                _language = value;
+                _cookieStorage?.SetItemAsync(LanguageCookieKey, value);
+            }
+        }
+
+        public void Initialization(IRequestCookieCollection cookies)
+        {
+            _language = cookies[LanguageCookieKey];
+        }
+
+        public void Bind(I18nConfig i18NConfig)
+        {
+            _language = i18NConfig.Language;
+        }
+    }
+}
---
+++ src/Component/BlazorComponent/I18n/I18nServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using BlazorComponent.Components;
+using System.Text.Json;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class I18nServiceCollectionExtensions
+    {
+        public static IServiceCollection AddI18n(this IServiceCollection services, List<LanguageSetting> languageSettings)
+        {
+            foreach (var seeting in languageSettings)
+            {
+                I18n.AddLang(seeting.Value, seeting.FilePath, seeting.IsDefaultLanguage);
+            }
+            services.AddScoped<I18n>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddI18n(this IServiceCollection services, string langugeSettingFilePath)
+        {
+            var languageSettings = JsonSerializer.Deserialize<List<LanguageSetting>>(File.ReadAllText(langugeSettingFilePath)) ?? throw new Exception("Failed to read i18n josn file data!");
+            services.AddI18n(languageSettings);
+
+            return services;
+        }
+    }
+
+    public class LanguageSetting
+    {
+        public LanguageSetting(string value, string filePath, bool isDefaultLanguage = false)
+        {
+            Value = value;
+            FilePath = filePath;
+            IsDefaultLanguage = isDefaultLanguage;
+        }
+
+        public string Value { get; set; }
+
+        public string FilePath { get; set; }
+
+        public bool IsDefaultLanguage { get; set; }
+    }
+}
